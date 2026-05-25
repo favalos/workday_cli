0377ce36350407a92cf20de2229bddd3bec4c9e2
Favalos <gavalosf@gmail.com>
2026-05-24 23:54:46 -0700

    Update install to v0.0.8

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 REPO="favalos/workday_cli"
-VERSION="v0.0.7"
+VERSION="v0.0.8"
 BINARY_URL="https://github.com/${REPO}/releases/download/${VERSION}/workday_cli"
 SKILL_URL="https://raw.githubusercontent.com/${REPO}/main/SKILL.md"
 
